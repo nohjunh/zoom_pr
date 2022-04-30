d0f4114c4d3037fb688f7256a0da3c724ead09b7
nohjunh <nsh042777@naver.com>
2022-04-30 14:32:46 +0900

    4-30

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,21 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = new WebSocket(`ws://${window.location.host}`); //Websocket이 프론트와 백엔드를 브라우저에서 연결해주는 것을 지원함. // 이렇게 써줌으로써 프론트와 백엔드가 연결됨
+//const socket= new webSocket("ws://localhost:3000")와 동일 //ws는 프로토콜, localhost:3000은 url
+// window.location하면 내가 어디에 있는지 알려줌 -> .host를 통해 localhost:3000 이 나오게 할 수 있다.
+// new WebSocket의 반환값을 socket변수에 저장하고 이 socket변수를 통해 백엔드와 실시간 통신을 할 수 있게 된다.
+
+socket.addEventListener("open", ()=>{ // callback function을 익명함수로 해서 프론트에서 소켓이 열렸다는 event를 받으면 다음 log를 띄움.
+    console.log("connectd to server"); 
+});
+
+socket.addEventListener("message", (message) => { // message event를 받으면 message값을 인자로 넣어서 해당 값 출력
+    console.log("New message: ", message.data, "from the Server"); // message의 data 값 출력
+});
+
+socket.addEventListener("close", ()=>{ // 서버와 연결 끊김.
+    console.log("disconnected from server");
+});
+
+// 10초뒤에 브라우저에서 백엔드로 data전송
+setTimeout( ()=> { //setTimeout 함수를 통해 첫번째 인자로 들어오는 함수를 두번째 인자값인 10초뒤에 수행.
+    socket.send("hello from the browser!");
+}, 10000); 
--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));// home.pug를 render해주는 route handler
 // 유저가 "홈"으로 get request를 보내면 (app.get("/")) template으로 반응(res.render("home"))
+// 유저가 해당 url로 가면, req와 res를 받고 response를 보냄
 
 app.get("/*", (req,res) => res.redirect("/")); // 유저가 "/아무거나" 요청하면 "/"으로 리다이렉트
 //유저가 어떤 페이지로 GET request를 보내도 redirect로 반응
@@ -26,13 +27,25 @@ const wss = new WebSocket.Server({ server });
 // "http://localhoost:3000" + "ws://localhoost:3000"
 
 
-function handleConnetion(socket){ // socket은 서버(나)와 브라우저의 연결
-    console.log(socket);
-}
+//websocket도 event가 있고 event가 발동될 때 사용할 function을 만들면 된다.
+//websocket은 listen할 특정한 event명이 있고 추가적인 정보를 받을 function이 있다.
 
-wss.on("connection", handleConnetion); // on method에서는 event가 발동하는걸 기다린다. 여기서 event는 connection
-                                       // connection 이벤트 발생시 handleConnetion function 수행
-                                       // on method는 socket을 통해 backend에 연결된 클라이언트의 정보를 제공함
+// on method에서는 event가 발동하는걸 기다린다. 여기서 event는 connection
+// connection 이벤트 발생시 handleConnetion function 수행 -> 그러나, 익명함수를 통해 따로 handleConnetion function을 선언하지 않아도 on method안에서 다 처리 가능.
+// on method는 socket을 통해 backend에 연결된 클라이언트의 정보를 제공함
+// 익명함수를 통해 callback fuction을 on method문에서 바로 참조할 수 있도록 한다. -> socket이 어떤 상태인지 쉽게 파악할 수 있음
+wss.on("connection", (socket) => { 
+    //console.log(socket); => socket값을 인자로 받아서 console.log(socket)을 수행하는 익명함수를 포함한 on method
+    console.log("Connected to browser"); // 브라우저가 서버에 연결된 클라이언트라고 생각하면 됨.
+    socket.on("close", ()=>{ // backend에서 event를 listen할 때는 on method 사용. -> close event를 받고 익명함수 수행
+        console.log("disconnected from the browser"); // 클라이언트의 web 브라우저가 꺼지면 이 문자열을 콘솔에 출력
+    });
+    socket.on("message", (message)=>{     // message event등록 // event를 발생시켜 전송받은 message인자값을 콘솔에 출력
+        console.log(message);
+    });
+    socket.send("hello!!!"); // socket으로 data를 보냄.
+});
+
 server.listen(3000, handleListen);
 
 
