5d7b26326ade9fe2d2acf1c669be10951e556257
nohjunh <nsh042777@naver.com>
2022-05-15 20:50:42 +0900

    #3.0 User Video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,59 +1,53 @@
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
-room.hidden = true;
 
-let roomName;
+//  stream은 유저로부터 비디오와 오디오가 결합한 것
+let myStream;
+let muted = false; // 처음에는 소리가 나는 상태로 시작할거기에 false로 시작
+let cameraOff = false; //마찬가지로 카메라off도 false로 시작
 
-function addMessage(message) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.appendChild(li);
+// getMedia() API
+async function getMedia() {
+  try {
+    // navigator.mediaDevices.getUserMedia는 유저의 "유저미디어 string"을 줌
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true, // 내가 얻고 싶은 거 => audio
+      video: true, // 내가 얻고 싶은 거 => video
+    });
+    // const myFace = document.getElementById("myFace");
+    myFace.srcObject = myStream; // MyFace에 myStream에 저장된 값을 넣는다.
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function handleMessageSubmit(event) {
-  event.preventDefault();
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-  socket.emit("new_message", input.value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
-  input.value = "";
+getMedia(); //
+
+function handleMuteClick() { // "mute" == 음소거
+  if (!muted) { // 음소거가 되어 있지 않다면,
+    muteBtn.innerText = "Unmute"; // 음소거가 되어 있지 않으니까 버튼의 텍스트는 Unmute로 함.
+                                  // 클릭하면 음소거가 되게 함.
+    muted = true; // 음소거가 아닌 상태에서 버튼을 누르면 음소거가 됨.
+  } else { // 음소거라면
+    muteBtn.innerText = "Mute"; // 음소거가 되어있는 상태라면 버튼의 텍스트는 Mute
+    muted = false;
+  }
+}
+function handleCameraClick() {
+  if (cameraOff) { // 카메라가 꺼져있다면, 버튼을 클릭할때 카메라가 켜지게 함.
+    cameraBtn.innerText = "Turn Camera Off"; //버튼텍스트를 Off로 해서 카메라가 꺼져있음을 알려줌.
+    cameraOff = false; // 카메라가 Off고 우리가 on을 하면 cameraOff 값은 false가 되어야 한다.
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
 }
 
-function showRoom() {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}`;
-  const msgForm = room.querySelector("#msg");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-}
 
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-socket.on("welcome", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${user} arrived!`);
-});
-
-socket.on("bye", (left, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${left} left ㅠㅠ`);
-});
-
-socket.on("new_message", addMessage);
+//버튼을 클릭했다는 event인 click event가 발생하면 뒤에 callback 함수 실행
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
