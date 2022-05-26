522fb7b2102b013dcd549667f4d2c798f202a7a9
nohjunh <nsh042777@naver.com>
2022-05-26 17:11:11 +0900

    #3.2 Camera Switch 내용 수정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,12 +13,13 @@ async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    // getVideoTracks()를 통해 어떤 카메라가 선택되었는지 알 수 있다.
     const currentCamera = myStream.getVideoTracks()[0];// videotracks이 첫 번째 track을 가져온다.
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId; // camera는 forEach 인자
       option.innerText = camera.label;
-      // 만약 카메라 option이 현재 선택된 카메라와 같은 label을 가지고 있다면
+      // 만약 카메라 option이 현재 선택된 카메라와 같은 label을 가지고 있다면(카메라 객체 옵션에 있음)
       // 이게 현재 사용되고 있는 카메라이다.
       if (currentCamera.label === camera.label) { // currentCamera의 label이 camera의 label과 같다면
         option.selected = true; // 그 옵션은 선택된거
@@ -35,7 +36,7 @@ async function getMedia(deviceId) { // getMedia가 argument를 하나 받도록 
   const initialConstrains = { // 초기constrains 설정 (deviceID없이 처음에 getMedia를 호출했을 때 실행)
                               // cameras를 만들기 전에 호출됨.
     audio: true,
-    video: { facingMode: "user" },
+    video: { facingMode: "user" }, //유저가 핸드폰에서 이 코드를 열 때 브라우저가 셀카를 선택
   };
   // deviceID가 있을 때 cameraConstraints를 가진다.
   const cameraConstraints = {
@@ -43,7 +44,7 @@ async function getMedia(deviceId) { // getMedia가 argument를 하나 받도록 
     // deviceID가 있고 exact: deviceId로 하면 브라우저가 이 deviceID만 사용하고 만약 찾지못하면
     // 비디오가 표시되지 않는다.
     // 유저로부터 getMedia함수의 인자로 deviceID를 받았기 때문.
-    video: { deviceId: { exact: deviceId } },
+    video: { deviceId: { exact: deviceId } }, // video를 deviceID로 함
   };
   try {
     // getUserMedia 함수를 호출할 때 마다 constraints를 보내야 한다.
