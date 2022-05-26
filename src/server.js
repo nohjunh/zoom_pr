f1dc1ebcefa187718a320121753253326f5151d7
nohjunh <nsh042777@naver.com>
2022-05-26 17:26:18 +0900

    #3.4 Rooms

--- src/server.js
+++ src/server.js
@@ -13,11 +13,12 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+
 wsServer.on("connection", (socket) => {
   socket.on("join_room", (roomName, done) => {
-    socket.join(roomName);
-    done();
-    socket.to(roomName).emit("welcome");
+    socket.join(roomName); // 프론트로부터 받은 roomName값 (input.value) 으로 방에 참가
+    done(); // 프론트의 startMEdia함수 수행
+    socket.to(roomName).emit("welcome"); // 룸에 있는 모두에게 welcome event 발생시킴
   });
 });
 
